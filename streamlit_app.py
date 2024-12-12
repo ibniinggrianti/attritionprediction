7ad3b91fbb6397cde20966c2ab166952d795ddca
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-12 11:26:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,53 +16,6 @@ st.write("Is your job worth keeping? Should you stay? Or just leave? Let's try!"
 data = pd.read_csv("https://raw.githubusercontent.com/ibniinggrianti/attritionprediction/refs/heads/master/IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv")  # Replace with your actual CSV file name
 
 with st.expander('Data Visualization'):
-  st.write("This is detail visualization on every features.")
-  # Employee Attrition Counts Visualization
-  with st.expander("Employee Attrition Rate"):
-    st.write("Detail visualization.")
-    if "Attrition" in data.columns:
-            attrition_rate = data["Attrition"].value_counts()
-              
-            # Creating two columns for side-by-side plots
-            col1, col2 = st.columns(2)
-              
-            # Bar Plot in the first column
-            with col1:
-                st.info("### Employee Attrition Counts")
-                fig, ax = plt.subplots(figsize=(4, 3))
-                sns.barplot(x=attrition_rate.index, y=attrition_rate.values, palette=["#1d7874", "#8B0000"], ax=ax)
-                #ax.set_title("Employee Attrition Counts", fontweight="black", size=20, pad=20)
-              
-                # Adding value annotations to the bars
-                for i, v in enumerate(attrition_rate.values):
-                    ax.text(i, v, v, ha="center", fontweight='black', fontsize=10)
-              
-                st.pyplot(fig)
-              
-            # Pie Chart in the second column
-            with col2:
-                st.info("### Employee Attrition Rate")
-                fig, ax = plt.subplots(figsize=(6, 6))
-                ax.pie(
-                    attrition_rate, 
-                    labels=["No", "Yes"], 
-                    autopct="%.2f%%", 
-                    textprops={"fontweight": "black", "size": 18},
-                    colors=["#1d7874", "#AC1F29"],
-                    explode=[0, 0.1], 
-                    startangle=90
-                )
-                center_circle = plt.Circle((0, 0), 0.3, fc='white')
-                fig.gca().add_artist(center_circle)
-                #ax.set_title("Employee Attrition Rate", fontweight="black", size=20, pad=10)
-              
-                st.pyplot(fig)
-              
-    else:
-           st.info("Please upload a CSV file to start the analysis.")
-  st.write("Employees Distribution by Gender.")
-
-with st.expander('Data Visualization'):
     st.write("This is detail visualization on every feature.")
     
     # Employee Attrition Counts Visualization
@@ -111,4 +64,3 @@ with st.expander('Data Visualization'):
             st.info("Please upload a CSV file to start the analysis.")
     
     st.write("Employees Distribution by Gender.")
-
