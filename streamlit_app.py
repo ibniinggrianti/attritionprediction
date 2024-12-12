b3ec034faed169ce3b26fc7ec437035cf2e1753e
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-12 11:54:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,7 +138,7 @@ with st.expander('Data Visualization'):
             # Add a white circle at the center to make it a donut chart
             center_circle = plt.Circle((0, 0), 0.4, fc='white')
             fig.gca().add_artist(center_circle)
-            ax.set_title("Employees by Marital Status", fontweight="black", size=20, pad=20)
+            #ax.set_title("Employees by Marital Status", fontweight="black", size=20, pad=20)
             st.pyplot(fig)
 
         # Visualization for Attrition Rate by Marital Status (Bar plot)
@@ -147,9 +147,9 @@ with st.expander('Data Visualization'):
             new_df = data[data["Attrition"] == "Yes"]
             value_2 = new_df["MaritalStatus"].value_counts()
             attrition_rate = np.floor((value_2 / value_1) * 100).values
-            fig, ax = plt.subplots(figsize=(6, 6))
+            fig, ax = plt.subplots(figsize=(4, 3))
             sns.barplot(x=value_2.index, y=value_2.values, palette=["#11264e", "#6faea4", "#FEE08B", "#D4A1E7", "#E7A1A1"], ax=ax)
-            ax.set_title("Attrition Rate by Marital Status", fontweight="black", size=20, pad=20)
+            #ax.set_title("Attrition Rate by Marital Status", fontweight="black", size=20, pad=20)
 
             # Add text annotations for each bar
             for index, value in enumerate(value_2):
