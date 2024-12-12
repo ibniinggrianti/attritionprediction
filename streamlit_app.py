fe2eec2c0329f188773c44b692551524134accbc
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-12 10:05:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('Attrition Prediction')
 
 st.info("Is your job worth keeping? Should you stay? Or just leave? Let's try!")
 
-df = pd.read.csv('https://raw.githubusercontent.com/ibniinggrianti/attritionprediction/refs/heads/master/IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/ibniinggrianti/attritionprediction/refs/heads/master/IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv')
 df
