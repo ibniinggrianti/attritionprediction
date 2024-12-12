a434dae99ec45b9ce627f36471b3e27ac22547bd
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-12 09:53:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Attrition Prediction')
 
-st.info ('Is your job worth keeping? Should you stay? Or just leave? Let's try!')
+st.info("Is your job worth keeping? Should you stay? Or just leave? Let's try!")
