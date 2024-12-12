1ad787a3918408a16003efc5dc66d408cc4dabdf
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-12 12:09:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,7 +197,7 @@ with st.expander('Statistics by Job'):
 
             # Add text annotations for each bar
             for index, value in enumerate(value_2):
-                ax.text(index, value, f"{value} ({int(attrition_rate[index])}%)", ha="center", va="bottom", size=15, fontweight="black")
+                ax.text(index, value, f"{value} ({int(attrition_rate[index])}%)", ha="center", va="bottom", size=10, fontweight="black")
             st.pyplot(fig)
 
   else:
