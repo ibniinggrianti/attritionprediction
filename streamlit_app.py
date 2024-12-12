e8615bb7669cd26a57afd1740e22a74a94db7265
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-12 10:05:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Attrition Prediction')
 
 st.info("Is your job worth keeping? Should you stay? Or just leave? Let's try!")
+
+df = pd.read.csv('https://raw.githubusercontent.com/ibniinggrianti/attritionprediction/refs/heads/master/IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv')
+df
