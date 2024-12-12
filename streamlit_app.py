f71dd94015223b799c67abc42892297abcb7e386
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-12 11:07:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ data = pd.read_csv("https://raw.githubusercontent.com/ibniinggrianti/attritionpr
 
 with st.expander('Data Visualization'):
 # Employee Attrition Counts Visualization
-      with st.expander("Employee Attrition Rate"):
+  with st.expander("Employee Attrition Rate"):
               if "Attrition" in data.columns:
                       attrition_rate = data["Attrition"].value_counts()
               
