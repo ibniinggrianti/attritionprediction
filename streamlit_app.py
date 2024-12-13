ac1f25eb806d935d1901603f4ca0acd333d07652
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-13 08:34:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -695,7 +695,8 @@ with st.expander('Statistics by Job'):
       
 with st.sidebar:
   st.header('Input Features')
-  age = st.slider('Age', 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60)
+  age = st.slider("Select Age", min_value=18, max_value=60, value=25)
+  st.write(f"The selected age is {age}.")
 
   options = ["Male", "Female"]
   selection = st.pills("Gender", options, selection_mode="single")
