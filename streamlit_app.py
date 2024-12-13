a05e2692085827b1d48ef101afabe508b686b20c
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-13 10:21:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
+#from sklearn.model_selection import train_test_split
+#from sklearn.pipeline import make_pipeline
+#from sklearn.svm import SVC
+#from sklearn.preprocessing import StandardScaler
+#from sklearn.metrics import accuracy_score
 
 st.title('Attrition Prediction')
 
