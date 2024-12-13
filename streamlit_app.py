7a3f7c047bb3a8235a1efda7820feffeb11ecb2e
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-13 08:32:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -695,12 +695,10 @@ with st.expander('Statistics by Job'):
       
 with st.sidebar:
   st.header('Input Features')
+  age = st.slider('Age', 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60)
+
   options = ["Male", "Female"]
   selection = st.pills("Gender", options, selection_mode="single")
-  st.markdown(f"Your selected options: {selection}.")
-
-  options = ["20-30", "31-40", "41-50", "51-60"]
-  selection = st.pills("Age", options, selection_mode="single")
   st.markdown(f"Your selected options: {selection}.")
 
   options = ["Single", "Married", "Divorced"]
